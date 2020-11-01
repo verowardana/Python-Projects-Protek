d92ff0200feec1edd0e84290924b207db15e806e
verowardana <71622469+verowardana@users.noreply.github.com>
2020-11-01 23:55:54 +0700

    Create latihan01.py

---
+++ Python Project/Chapter 6/latihan01.py
@@ -0,0 +1,25 @@
+def isPythagoras(a,b,c):
+    if(a**2 + b**2 == c**2):
+        return True
+    else:
+        return False
+a = 3
+b = 4
+c = 5
+print(isPythagoras(a,b,c))
+
+a = 5
+b = 9
+c = 12
+print(isPythagoras(a,b,c))
+
+a = 8
+b = 6
+c = 10
+print(isPythagoras(a,b,c))
+
+a = 7
+b = 8
+c = 11
+print(isPythagoras(a,b,c))
+
